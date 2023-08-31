6c5a85a17cdeba4497ce5b302262d78cc4df64ae
matheus <matheusfarnetani@gmail.com>
2023-08-31 16:14:25 -0300

    Base for login complete

--- helpersdb.py
+++ helpersdb.py
@@ -1,12 +1,13 @@
 import sqlite3
 
 # Path to database
-db = "./static/database/finance.db"
+db = "./static/database/data.db"
 
 # Create connection to the database
 con = sqlite3.connect(db, check_same_thread=False)
 
 
+# Change sqlite3 return type, from list of tuples to list of dicts
 def dict_factory(cursor, row):
     fields = [column[0] for column in cursor.description]
     return {key: value for key, value in zip(fields, row)}
@@ -19,10 +20,20 @@ con.row_factory = dict_factory
 cur = con.cursor()
 
 
-def query_username(username: str):
-    """Query database for username"""
+def query_username(username: str) -> dict:
+    """Query database for username, and get all results;\n
+    Returns None if result is diferent than 1;\n
+    Returns dict, with corresponded user's data, if result is 1."""
     rows = cur.execute("SELECT * FROM users WHERE username = ?;", (username,))
+
+    # Select all from query
     rows = cur.fetchall()
+
+    # If it is different than 1.
+    # There are multiple users with same username or there is none.
+    # Both were considered errors.
     if len(rows) != 1:
         return None
+    
+    # Return first dict
     return rows[0]
