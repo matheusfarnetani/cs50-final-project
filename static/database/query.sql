51d6965c2841896ca4da09dfde16c474df36c8fa
matheus <matheusfarnetani@gmail.com>
2023-08-31 16:35:35 -0300

    add query.sql

---
+++ static/database/query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    user_type TEXT CHECK(user_type IN ('student', 'collaborator', 'visitor'))
+);
+CREATE UNIQUE INDEX username ON users (username);
